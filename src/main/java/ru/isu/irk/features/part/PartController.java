7423aeeb5e129d4dac616eb12f051deda2e09e5d
MrLyasichka007 <mrfox474@gmail.com>
2024-12-03 23:27:12 +0800

    Rebuilded structure

---
+++ src/main/java/ru/isu/irk/features/part/PartController.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ru.isu.irk.features.part;
+
+/**
+ *
+ * @author Nikita
+ */
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// import ru.isu.irk.features.part.*;
+// import ru.isu.irk.features.PartService.*;
+
+@Controller
+@RequestMapping("/parts")
+public class PartController {
+    
+    private PartService partService;
+
+    @Autowired
+    public PartController(PartService partService) {
+        this.partService = partService;
+    }
+
+    @ModelAttribute("partTypes")
+    public Map<String, String> getPartTypes(){
+        Map<String, String> res = new HashMap<>();
+        res.put("Body", "Корпус");
+        res.put("Plate", "Пластина");
+        res.put("Cang", "Цанга");
+        res.put("Cang_inner", "Цанг. патрон");
+        res.put("Holder", "Б. держатель");
+        return res;
+    }
+
+    @GetMapping
+    public String listParts(Model model) {
+        List<Part> parts = partService.getAllParts();
+        model.addAttribute("parts", parts);
+        return "listParts";
+    }
+    
+    @GetMapping("/add")
+    public String addPart(Model model) {
+        Part part = new Part();
+        model.addAttribute("parts", part);
+        return "addPart";
+    }
+    
+    @PostMapping("/save")
+    public String savePart(@ModelAttribute("parts") Part part) {
+        partService.savePart(part);
+        return "redirect:/parts";
+    }
+    
+    @GetMapping("/edit/{partId}")
+    public String editPart(@PathVariable("partId") Long partId, Model model) {
+        Part part = partService.getPartById(partId);
+        model.addAttribute("part", part);
+        return "editPart";
+    }
+    
+    @PostMapping("/update")
+    public String updatePart(@ModelAttribute("part") Part part) {
+        partService.updatePart(part);
+        return "redirect:/parts";
+    }
+
+    @GetMapping("/get/{partId}")
+    @ResponseBody
+    public Part getPart(@PathVariable("partId") Part part){
+        return part;
+    }
+}
---
+++ src/main/java/ru/isu/irk/features/technitian/Technitian.java
@@ -0,0 +1,81 @@
+package ru.isu.irk.features.technitian;
+
+import ru.isu.irk.features.techCard.*;
+import ru.isu.irk.features.part.*;
+import ru.isu.irk.features.instrument.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+
+@Entity
+@Table(name = "technitian")
+public class Technitian {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @OneToMany(mappedBy = "technitian", cascade = CascadeType.ALL)
+    private List<TechCard> techCards;
+    // Add other variables as needed
+    
+    // Constructor
+    public Technitian(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Technitian() {
+
+    }
+    
+    
+    
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    //MOVE TO CONTROLLER ALL BELOW
+    // Method to add a new TechCard to the system
+    public void addTechCard(TechCard techCard) {
+        techCards.add(techCard);
+        //techCard.setTechnitian(this);
+    }
+    
+    // Method to add a new Part to the system
+    public void addPart(Part part) {
+        // Code to add the Part to the system
+    }
+    
+    // Method to add a new Instrument to the system
+    public void addInstrument(Instrument instrument) {
+        // Code to add the Instrument to the system
+    }
+}
